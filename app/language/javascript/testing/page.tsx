6bfe8e534d439fa07364c32f14c8b1e462e1ef04
Hendrik Röttgers <hrottgers@protonmail.com>
2026-08-12 13:45:35 +0200

    UPDATED: JS/testing

--- app/language/javascript/testing/page.tsx
+++ app/language/javascript/testing/page.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import Link from "next/link";
+import { useRef } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
+import styles from "./button.module.css";
 
 export default function Page() {
   return (
@@ -8,6 +12,7 @@ export default function Page() {
       <h2>Testing</h2>
       <TestingMocha />
       <TestingMochaAssert />
+      <br />
     </div>
   );
 }
@@ -32,26 +37,91 @@ function TestingMocha() {
 }
 
 function TestingMochaAssert() {
+  const hiddenElement = useRef<HTMLParagraphElement>(null);
+
+  const showMore = () => {
+    if (hiddenElement.current) {
+      hiddenElement.current.style.display =
+        hiddenElement.current.style.display === "none" ? "block" : "none";
+    }
+  };
+
   return (
     <div>
       <h4>Asserting outcomes</h4>
-      <p>Example test:</p>
+      <p>The following test suite test this simple script:</p>
+      <button id={styles.button} onClick={showMore}>
+        Script
+      </button>
+      <div ref={hiddenElement} style={{ display: "none" }}>
+        <SyntaxHighlighter
+          language="javascript"
+          style={dracula}
+        >{`// Define a rooster
+Rooster = {};
+
+// Return a morning rooster call
+Rooster.announceDawn = () => {
+  return "cock-a-doodle-doo!";
+};
+
+// Return hour as string
+// Throws Error if hour is not between 0 and 23 inclusive
+Rooster.timeAtDawn = (hour) => {
+  if (hour < 0 || hour > 23) {
+    throw new RangeError();
+  } else {
+    return hour.toString();
+  }
+};
+module.exports = Rooster;`}</SyntaxHighlighter>
+      </div>
       <SyntaxHighlighter
         language="javascript"
         style={dracula}
-      >{`const assert = require('assert');
+      >{`const assert = require("assert");
+const Rooster = require("../index");
 
-  describe('+', () => {
-    it('returns the sum of two values', () => {
-      // Setup
-	let expected = [3, 4, 7];
-	let sum = [3, 4];
+describe("Rooster", () => {
+  describe(".announceDawn", () => {
+    it("returns a rooster call", () => {
+      //Setup
+      const expected = "cock-a-doodle-doo!";
 
-      // Exercise
-	sum.push(3 + 4);
+      //Exercise
+      const result = Rooster.announceDawn();
 
-      // Verify
-    assert.deepStrictEqual(sum, expected);
+      //Verify
+      assert.strictEqual(result, expected);
+    });
+  });
+  describe(".timeAtDawn", () => {
+    it("returns its argument as a string", () => {
+      //Setup
+      const time = 7;
+      const expected = String(7);
+
+      //Exercise
+      const result = Rooster.timeAtDawn(time);
+
+
+      //Verify
+      assert.strictEqual(typeof result, typeof expected);
+    });
+    it("throws an error if passed a number less than 0", () => {
+      const hour = -1;
+
+      assert.throws(() => {
+        Rooster.timeAtDawn(hour);
+      }, RangeError);
+    });
+    it("throws an error if passed anumber greater than 23", () => {
+      const hour = 25;
+
+      assert.throws(() => {
+        Rooster.timeAtDawn(hour);
+      }, RangeError);
+    });
   });
 });`}</SyntaxHighlighter>
       <p>
