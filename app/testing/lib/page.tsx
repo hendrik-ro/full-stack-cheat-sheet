b88e6df91f74c2d502217ce8595329b5a33f7f09
Hendrik Röttgers <hrottgers@protonmail.com>
2026-08-12 15:18:24 +0200

    UPDATE: testing/lib

--- app/testing/lib/page.tsx
+++ app/testing/lib/page.tsx
@@ -15,6 +15,7 @@ function TestingChai() {
       <ul style={{fontSize:"1rem"}}>
         <li><Link target="_blank" rel="noopener noreference" href="https://www.chaijs.com/">Chai</Link> - assertion library for JavaScript</li>
         <li><Link target="_blank" rel="noopener noreference" href="https://mochajs.org/">Mocha</Link> - testing framework for JavaScript</li>
+        <li><Link target="_blank" rel="noopener noreference" href="https://sinonjs.org/">Sinon</Link> - library that includes fakes, spies and mocks that can be used with any testing framework</li>
       </ul>
     </div>
   )
