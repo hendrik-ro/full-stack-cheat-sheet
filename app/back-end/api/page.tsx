133c42e445636b9ac34350fe4b8429cc04ccb14f
Hendrik Röttgers <hrottgers@protonmail.com>
2026-08-16 22:42:23 +0200

    updated: app/back-end/api/rest

--- app/back-end/api/page.tsx
+++ app/back-end/api/page.tsx
@@ -14,6 +14,61 @@ export default function Page() {
           </Link>
           or Application Programming Interface are connections between two programs.
       </p>
+      <CRUD />
+      <Types />
+      <ResponseCodes />
+    </div>
+  );
+}
+
+function CRUD() {
+  return (
+    <div>
+      <h2>CRUD</h2>
+      <table>
+        <thead>
+          <tr>
+            <th style={{ color:"steelblue" }}>Operation</th>
+            <th style={{ color:"steelblue" }}>HTTP Method</th>
+            <th style={{ color:"steelblue" }}>SQL Statement</th>
+            <th style={{ color:"steelblue" }}>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Create</td>
+            <td>POST</td>
+            <td>INSERT</td>
+            <td>For creating a new resource</td>
+          </tr>
+          <tr>
+            <td>Read</td>
+            <td>GET</td>
+            <td>SELECT</td>
+            <td>For retrieving a resource</td>
+          </tr>
+          <tr>
+            <td>Update</td>
+            <td>PUT</td>
+            <td>UPDATE</td>
+            <td>For updating an existing resource</td>
+          </tr>
+          <tr>
+            <td>Delete</td>
+            <td>DELETE</td>
+            <td>DELETE</td>
+            <td>For deleting a resource</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+function Types() {
+  return (
+    <div>
+      <h2>Types of APIs</h2>
       <table>
         <thead>
           <tr>
@@ -56,5 +111,51 @@ export default function Page() {
         </tbody>
       </table>
     </div>
-  );
+  )
+}
+
+function ResponseCodes() {
+  return (
+    <div>
+      <h2>Response Codes</h2>
+      <table>
+        <thead>
+          <tr>
+            <th style={{ color:"steelblue" }}>Status Code</th>
+            <th style={{ color:"steelblue" }}>Meaning</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>200 (OK)</td>
+            <td>This is the standard response for successful HTTP requests.</td>
+          </tr>
+          <tr>
+            <td>201 (CREATED)</td>
+            <td>This is the standard response for an HTTP request that resulted in an item being successfully created.</td>
+          </tr>
+          <tr>
+            <td>204 (NO CONTENT)</td>
+            <td>This is the standard response for successful HTTP requests, where nothing is being returned in the response body.</td>
+          </tr>
+          <tr>
+            <td>400 (BAD REQUEST)</td>
+            <td>The request cannot be processed because of bad request syntax, excessive size, or another client error.</td>
+          </tr>
+          <tr>
+            <td>403 (FORBIDDEN)</td>
+            <td>The client does not have permission to access this resource.</td>
+          </tr>
+          <tr>
+            <td>404 (NOT FOUND)</td>
+            <td>The resource could not be found at this time. It is possible it was deleted, or does not exist yet.</td>
+          </tr>
+          <tr>
+            <td>500 (INTERNAL SERVER ERROR)</td>
+            <td>The generic answer for an unexpected failure if there is no more specific information available.</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
 }
--- app/back-end/api/rest/page.tsx
+++ app/back-end/api/rest/page.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div>
+      <h1>REST</h1>
       <p>
         <Link
           target="_blank"
           href="https://developer.mozilla.org/en-US/docs/Glossary/REST"
         >
           REST
-        </Link>
-        or Representational State Transfer is an architectural style for
-        designing networked applications.
+        </Link> or Representational State Transfer is an architectural style for designing networked applications.
       </p>
+      <p>A REST API is <em>stateless</em>, meaning the server does not store any client state between requests and vise versa. Client and server are unaware of each other.</p>
     </div>
   );
 }
