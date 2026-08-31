acffbe5b41d8ffbc3322ddb8c161c5b7e931b696
Hendrik Röttgers <hrottgers@protonmail.com>
2026-08-31 23:07:21 +0200

    Redux (#60)
    * Redux syntax
    
    * Redux React syntax

--- app/front-end/react/redux/page.tsx
+++ app/front-end/react/redux/page.tsx
@@ -9,6 +9,8 @@ export default function Page() {
       <Redux />
       <ReduxConcepts />
       <ReduxStore />
+      <ReduxSyntax />
+      <ReduxReact />
       <br />
     </div>
   );
@@ -145,6 +147,173 @@ function ReduxStore() {
         <li>The store`s reducer determines the next state.</li>
         <li>The user interface is updated.</li>
       </ol>
+    </div>
+  );
+}
+
+function ReduxSyntax() {
+  return (
+    <div>
+      <h2>Syntax</h2>
+      <SyntaxHighlighter
+        language="js"
+        style={dracula}
+      >{`import { createStore } from 'redux';
+
+const initialState = 0;
+const countReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+}`}</SyntaxHighlighter>
+      <p>
+        The method <code>.dispatch</code> allows us to alternate the stored
+        state:
+      </p>
+      <SyntaxHighlighter
+        language="js"
+        style={dracula}
+      >{`const store = createStore(countReducer);
+
+store.dispatch({ type: 'increment' });
+console.log(store.getState()); // prints 1
+store.dispatch({ type: 'increment' });
+console.log(store.getState()); // prints 2
+store.dispatch({ type: 'decrement' });
+console.log(store.getState()); // prints 1`}</SyntaxHighlighter>
+      <p>
+        <strong>Action creators</strong> can be used to simplify calling
+        actions:
+      </p>
+      <SyntaxHighlighter
+        language="js"
+        style={dracula}
+      >{`export const increment = () => {
+  return { type: 'increment' };
+}
+
+export const decrement = () => {
+  return { type: 'decrement' };
+  }
+store.dispatch(increment());`}</SyntaxHighlighter>
+      <h3>Subscription</h3>
+      <p>
+        The method <code>.subscribe</code> adds event listener and returns an
+        unsubscribe function:
+      </p>
+      <SyntaxHighlighter
+        language="js"
+        style={dracula}
+      >{`// lightSwitchReducer(), toggle(), and store omitted...
+
+        const reactToChange = () => {
+          console.log(\`The light was switched \${store.getState()}!\`);
+        }
+        const unsubscribe = store.subscribe(reactToChange);
+
+        store.dispatch(toggle());
+        // reactToChange() is called, printing:
+        // 'The light was switched off!'
+
+        store.dispatch(toggle());
+        // reactToChange() is called, printing:
+        // 'The light was switched on!'
+
+        unsubscribe();
+        // reactToChange() is now unsubscribed
+
+        store.dispatch(toggle());
+        // no print statement!
+
+        console.log(store.getState()); // Prints 'off'`}</SyntaxHighlighter>
+    </div>
+  );
+}
+
+function ReduxReact() {
+  return (
+    <div>
+      <h2>Redux in React</h2>
+      <h3>store.js</h3>
+      <SyntaxHighlighter
+        language="js"
+        style={dracula}
+      >{`import { createStore } from 'redux';
+
+export function increment() {
+  return {type: 'increment'}
+}
+
+export function decrement() {
+  return {type: 'decrement'}
+}
+
+const initialState = 0;
+
+const countReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+export const store = createStore(countReducer);`}</SyntaxHighlighter>
+      <h3>index.js</h3>
+      <SyntaxHighlighter
+        language="js"
+        style={dracula}
+      >{`import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.js";
+import { store } from "./store.js";
+
+const root = createRoot(document.getElementById("app"));
+
+const render = () => {
+  root.render(<App state={store.getState()} dispatch={store.dispatch} />);
+};
+
+render();
+
+store.subscribe(render);`}</SyntaxHighlighter>
+      <h3>App.js</h3>
+      <SyntaxHighlighter
+        language="js"
+        style={dracula}
+      >{`import React from "react";
+import { increment, decrement } from "./store";
+
+export default function App({ state, dispatch }) {
+  // Dispatchers:
+  const incrementerClicked = () => {
+    dispatch(increment());
+  };
+
+  const decrementerClicked = () => {
+    dispatch(decrement())
+  };
+
+  return (
+    <main>
+      <p id="counter">{state}</p>
+      <button id="incrementer" onClick={incrementerClicked}>
+        +
+      </button>
+      <button id="decrementer" onClick={decrementerClicked}>
+        -
+      </button>
+    </main>
+  );
+}`}</SyntaxHighlighter>
     </div>
   );
 }
